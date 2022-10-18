db3e1a361906ec8708594911ead900c25be76b68
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2022-10-18 17:16:31 +0200

    There were no url issues...

--- src/bot/uploadImage.ts
+++ src/bot/uploadImage.ts
@@ -5,7 +5,8 @@ export const imageApiUrl = "https://freeimage.host/api/1/upload";
 export const placeholder = "https://prod.cdn.bbaws.net/TDC_Blockbuster_-_Production/18/980/PP-7302_bd-w-superhigh_orig.jpg";
 
 export const uploadImage = async (location: string): Promise<string|null> => {
-    const imageLatest = `${process.env.HOSTNAME}/images/${location.substring(location.lastIndexOf("/")+1)}`;
+    const locationfilename = location.substring(location.lastIndexOf("/")+1);
+    const imageLatest = `${process.env.HOSTNAME}/${locationfilename}`;
     const fullApiUrl = `${imageApiUrl}?key=${process.env.FREE_IMAGE_HOST_TOKEN}&source=${imageLatest}`;
     logger.info(`free-image api request: ${fullApiUrl}`);
     const value = await axios.get(fullApiUrl);
