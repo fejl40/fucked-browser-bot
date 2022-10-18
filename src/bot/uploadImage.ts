0cd785760c85d960eff323799f488cb7144193e0
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2022-10-18 17:07:22 +0200

    Added log message that details full api call for debugging.

--- src/bot/uploadImage.ts
+++ src/bot/uploadImage.ts
@@ -5,9 +5,9 @@ export const imageApiUrl = "https://freeimage.host/api/1/upload";
 export const placeholder = "https://prod.cdn.bbaws.net/TDC_Blockbuster_-_Production/18/980/PP-7302_bd-w-superhigh_orig.jpg";
 
 export const uploadImage = async (location: string): Promise<string|null> => {
-    logger.info(`uploadImage location: ${location}`)
     const imageLatest = `${process.env.HOSTNAME}${location.substring(location.lastIndexOf("/"))}`;
     const fullApiUrl = `${imageApiUrl}?key=${process.env.FREE_IMAGE_HOST_TOKEN}&source=${imageLatest}`;
+    logger.info(`free-image api request: ${fullApiUrl}`);
     const value = await axios.get(fullApiUrl);
     if (value.status === 200) {
         const url = value.data?.image?.url;
