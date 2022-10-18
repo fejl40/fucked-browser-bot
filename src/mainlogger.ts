65b3ab8789c1dcd689352d24f0b02389e9f895ee
Mik Kröis <mkp@nine.dk>
2022-10-18 11:02:19 +0200

    feat: logging to a file

--- src/mainlogger.ts
+++ src/mainlogger.ts
@@ -1,6 +1,25 @@
-import { Logger } from "tslog";
+import { ILogObject, Logger } from "tslog";
+import { appendFileSync }  from "fs";
 
 const logger: Logger = new Logger({
     name: "MainLogger"
     });
+
+    logger.attachTransport(
+        {
+          silly: logToTransport,
+          debug: logToTransport,
+          trace: logToTransport,
+          info: logToTransport,
+          warn: logToTransport,
+          error: logToTransport,
+          fatal: logToTransport,
+        },
+        "debug"
+      );
+
+function logToTransport(LogObject: ILogObject) {
+    appendFileSync("logFile.txt", JSON.stringify(LogObject)+ "\r\n")
+}
+
   export { logger };
